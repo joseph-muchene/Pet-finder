1670c7291d652afb0789bf021a2964a14244af7b
joseph muchene <66246216+joseph-muchene@users.noreply.github.com>
2023-01-25 18:57:29 +0300

    changes

--- app.py
+++ app.py
@@ -1,11 +1,76 @@
 from flask import Flask
+from flask import render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-
+from sqlalchemy import Column, Integer, DateTime, Boolean
+import datetime
 app = Flask(__name__)
+db = SQLAlchemy()
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///petFinder.db"
+
+# initialize the app with the extension
+db.init_app(app)
+
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    found = Column(Boolean, default=False)
+
+    def __init__(self):
+        return f"{id} {self.name}"
+
 
 @app.route("/")
 def index():
-    return '<h1>Hello world</h1>'
+    pets = db.session.execute(db.select(Pet).order_by(Pet.name)).scalars()
+    return render_template('index.html', pets=pets)
+
+
+@app.route('/pet/create')
+def pet_create():
+    if request.method == "POST":
+        pet = Pet(
+            name=request.form["name"],
+            age=request.form["age"],
+            created_date=request.form["created_date"],
+            found=request.form["found"]
+
+        )
+
+        db.session.add(pet)
+        db.session.commit()
+        # redirect user to homepage
+        return redirect(url_for("index"))
+
+    return render_template('index.html')
+
+
+@app.route("/pet/<int:id>")
+def user_detail(id):
+    pet = db.get_or_404(Pet, id)
+    return render_template("detail.html", pet=pet)
+
+    # with app.app_context():
+    #     db.create_all()
+
+
+@app.route("/pet/<int:id>/delete", methods=["GET", "POST"])
+def user_delete(id):
+    pet = db.get_or_404(Pet, id)
+
+    if request.method == "POST":
+        db.session.delete(pet)
+        db.session.commit()
+        return redirect(url_for("user_list"))
+
+    return render_template("user/delete.html", pet=pet)
+
 
 if __name__ == '__main__':
+
     app.run()
